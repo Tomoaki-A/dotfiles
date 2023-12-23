0cfae3d9ae150ee53834f5c5c1353e13e391addb
Tomoaki Asano <exsrx97@gmail.com>
2023-12-23 10:16:23 +0900

    Upd coc settings

--- nvim/lua/coc.lua
+++ nvim/lua/coc.lua
@@ -7,3 +7,11 @@ vim.g.coc_global_extensions = {
   'coc-lists',
   'coc-copilot'
 }
+
+vim.cmd([[
+  inoremap <silent><expr> <Enter> coc#pum#visible() ? coc#pum#confirm() : "\<Enter>"
+  inoremap <silent><expr> <C-j> coc#pum#visible() ? coc#pum#next(1) : "\<C-j>"
+  inoremap <silent><expr> <C-k> coc#pum#visible() ? coc#pum#prev(1) : "\<C-k>"
+  inoremap <silent><expr> <Enter> coc#pum#visible() ? coc#pum#confirm() : "\<Enter>"
+  inoremap <silent><expr> <Esc> coc#pum#visible() ? coc#pum#cancel() : "\<Esc>"
+]])
