7457d2a14ec6aff4b1db748c2db6662a1a2106c8
Tomoaki Asano <exsrx97@gmail.com>
2023-12-23 10:16:51 +0900

    Add syntax apply language

--- nvim/lua/syntax.lua
+++ nvim/lua/syntax.lua
@@ -1,8 +1,17 @@
 require'nvim-treesitter.configs'.setup ({
   ensure_installed = {
     "lua",
+    "bash",
+    "vim",
+    "html",
+    "css",
+    "scss",
+    "go",
+    "json",
     "typescript",
+    "typoscript",
     "tsx",
+    "vue",
   },
  highlight = {
   enable = true,
