bd7e758a19c1648fc607bd169f8eb8bb1518d798
Tomoaki Asano <exsrx97@gmail.com>
2023-12-31 21:37:59 +0900

    Add tree keymap

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -2,6 +2,7 @@
 -- nvim-tree
 vim.keymap.set("n", "<Space>tt", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
 vim.keymap.set("n", "<Space>tf", ":NvimTreeFocus<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<Space>tp", [[:NvimTreeFindFile<cr>:lua require'nvim-tree.api'.fs.copy.relative_path()<cr>]], {silent = true, noremap = true})
 vim.keymap.set('n', '<Space>tc', require('nvim-tree.api').fs.print_clipboard, {noremap = true, silent = true})
 
 -- telescope
