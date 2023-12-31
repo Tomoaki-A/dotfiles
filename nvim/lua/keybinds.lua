618ba33002beee2bd84c2b541e771cfe0bcaac84
Tomoaki Asano <exsrx97@gmail.com>
2023-12-31 23:07:17 +0900

    Upd keymaps

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,10 +1,11 @@
 
 -- nvim-tree
 vim.keymap.set("n", "<Space>tt", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
-vim.keymap.set("n", "<Space>tf", ":NvimTreeFocus<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<Space>tf", ":NvimTreeFindFile<cr>", {silent = true, noremap = true})
 vim.keymap.set("n", "<Space>tp", [[:NvimTreeFindFile<cr>:lua require'nvim-tree.api'.fs.copy.relative_path()<cr>]], {silent = true, noremap = true})
 vim.keymap.set('n', '<Space>tc', require('nvim-tree.api').fs.print_clipboard, {noremap = true, silent = true})
 
 -- telescope
 vim.keymap.set('n', '<Space>ff', require('telescope.builtin').find_files, {})
+vim.keymap.set('n', '<Space>fb', require('telescope.builtin').buffers, {})
 vim.keymap.set('n', '<Space>fg', require('telescope.builtin').live_grep, {})
