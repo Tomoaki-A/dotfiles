b0d6331b380aab7421437e03386033d0f9247de0
Tomoaki Asano <exsrx97@gmail.com>
2024-01-05 19:38:43 +0900

    Upd keybinds

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -2,10 +2,10 @@
 vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
 
 -- nvim-tree
-vim.keymap.set("n", "<Space>tt", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
-vim.keymap.set("n", "<Space>tf", ":NvimTreeFindFile<cr>", {silent = true, noremap = true})
-vim.keymap.set("n", "<Space>tp", [[:NvimTreeFindFile<cr>:lua require'nvim-tree.api'.fs.copy.relative_path()<cr>]], {silent = true, noremap = true})
-vim.keymap.set('n', '<Space>tc', require('nvim-tree.api').fs.print_clipboard, {noremap = true, silent = true})
+vim.keymap.set("n", "<Space>at", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<Space>af", ":NvimTreeFindFile<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<Space>ap", [[:NvimTreeFindFile<cr>:lua require'nvim-tree.api'.fs.copy.relative_path()<cr>]], {silent = true, noremap = true})
+vim.keymap.set('n', '<Space>ac', require('nvim-tree.api').fs.print_clipboard, {noremap = true, silent = true})
 
 -- telescope
 vim.keymap.set('n', '<Space>ff', require('telescope.builtin').find_files, {})
