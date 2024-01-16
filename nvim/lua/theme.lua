4cb0e16b09a735aa1b7a3cacc006dea3dbb7d051
Tomoaki Asano <exsrx97@gmail.com>
2024-01-17 01:09:29 +0900

    :lipstick: Update error highlighting colors

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -4,3 +4,9 @@ end
 
 vim.cmd('set background=dark')
 vim.cmd('colorscheme github_dark_colorblind')
+
+vim.api.nvim_set_hl(0, "DiagnosticError", { fg="#ff0000" })
+vim.api.nvim_set_hl(0, "ErrorMsg", { fg="#ff0000" })
+vim.api.nvim_set_hl(0, "Error", { fg="#ff0000" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg="#ff0000" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline=true, fg="#ff0000" })
