263cd56f16b97a9f7be6ff58cbc6df28c68756ef
Tomoaki Asano <a.tomoaki.dev@gmail.com>
2025-06-01 13:37:18 +0900

    :sparkles: Add new alias for aider model

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -5,4 +5,5 @@ alias cg="node ~/template_file_creator/dist/index.js"
 
 alias t="tmux a"
 
-alias ai="aider --model gpt-4o-mini"
+alias aig="aider --model gpt-4o-mini"
+alias aic="aider --model anthropic/claude-sonnet-4-20250514"
