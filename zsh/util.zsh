c00446b329fe4ec510cd94c40d316e7dc70fd8a0
Tomoaki Asano <a.tomoaki.dev@gmail.com>
2025-06-01 13:37:50 +0900

    :sparkles: Update model

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -6,4 +6,4 @@ alias cg="node ~/template_file_creator/dist/index.js"
 alias t="tmux a"
 
 alias aig="aider --model gpt-4o-mini"
-alias aic="aider --model anthropic/claude-sonnet-4-20250514"
+alias aic="aider --model sonnet"
