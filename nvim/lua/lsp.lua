2ef3d86cf054d9f977880229f449d9a34c8b8800
Tomoaki Asano <a.tomoaki.dev@gmail.com>
2025-10-08 23:31:46 +0900

    :sparkles: Reorganize LSP settings and capabilities

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -10,26 +10,8 @@ cmp.setup({
   }),
 })
 
+local path = require("mason-core.path")
 require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = { "ts_ls","biome","eslint","tailwindcss","typos_lsp","pyright", "ruff" },
-}
-require('mason-lspconfig').setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    }
-  end,
-}
-
--- typo-lspをWarningレベルで起動
-require('lspconfig').typos_lsp.setup({
-    cmd_env = { RUST_LOG = "Warning" },
-    init_options = {
-        config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
-        diagnosticSeverity = "Warning"
-    }
-})
 
 -- tsserverの設定
 require('lspconfig').ts_ls.setup({
@@ -38,4 +20,43 @@ require('lspconfig').ts_ls.setup({
         importModuleSpecifierPreference = 'non-relative', -- 絶対パス優先
       }
     }
+})
+
+-- biomeの設定
+require('lspconfig').biome.setup({
+  cmd = { path.bin_prefix("biome"), "lsp-proxy" },
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  on_attach = function(client, bufnr)
+    -- デバッグ用
+    vim.notify("Biome attached with formatting: " .. tostring(client.server_capabilities.documentFormattingProvider))
+  end
+})
+-- eslintの設定
+require('lspconfig').eslint.setup({
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+-- tailwindcssの設定
+require('lspconfig').tailwindcss.setup({
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+-- pyrightの設定
+require('lspconfig').pyright.setup({
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+-- ruffの設定
+require('lspconfig').ruff_lsp.setup({
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+-- typo-lspをWarningレベルで起動
+require('lspconfig').typos_lsp.setup({
+    cmd_env = { RUST_LOG = "Warning" },
+    init_options = {
+        config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
+        diagnosticSeverity = "Warning"
+    },
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
